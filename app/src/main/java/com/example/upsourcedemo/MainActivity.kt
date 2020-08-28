62a4f2ba1b421851623c3c37e5c048e14dcd966a
单黛丰 <shandaifeng@aecg.com.cn>
2020-08-28 18:02:36 +0800

    添加按钮点击

--- app/src/main/java/com/example/upsourcedemo/MainActivity.kt
+++ app/src/main/java/com/example/upsourcedemo/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.upsourcedemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_hello_world.setOnClickListener {
+            Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+        }
     }
 }
